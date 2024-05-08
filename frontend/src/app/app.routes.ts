3fd683f0f7afb8e1ba1d6659e9ce4d48e9b464ae
Jakub Osełka <95690893+Kosiarsky@users.noreply.github.com>
2024-05-08 12:56:42 +0200

    Add first components and routing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainPageComponent },
+    { path: 'main', component: MainPageComponent },
+    { path: '**', pathMatch: 'full', 
+        component: NotFoundComponent },
+  ];
